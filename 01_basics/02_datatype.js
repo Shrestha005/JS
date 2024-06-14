240a2304b69d2c1cf7f31a4cc41ba0524529c117
Shrestha Mukherjee <134909641+Shrestha005@users.noreply.github.com>
2024-06-14 19:22:07 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// alert(3+3) //not possible as we are using nodejs not browser
+
+console.log("Shrestha");
+
+let name = "shrestha"
+let age = 20
+let isLoggedIn = false
+ 
+console.log(typeof "shres");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
+
+
