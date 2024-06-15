73cd04e8668cf178e2d95dbf0a52d6608c9e861f
Shrestha Mukherjee <134909641+Shrestha005@users.noreply.github.com>
2024-06-15 11:35:22 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score = "33"
+
+// console.log(typeof score);
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+// "33" = 33
+// "33abc" = NaN
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 is true , 0 is false
+// "" is false
+// "abc" is true
+
+//OPERATION:
+
+let value = 3
+let negValue = -value
+console.log(negValue);
