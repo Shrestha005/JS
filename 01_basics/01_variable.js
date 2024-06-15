7d5d82736a810bf7afd41e1bdb8b34a9047d317d
Shrestha Mukherjee <134909641+Shrestha005@users.noreply.github.com>
2024-06-15 06:31:13 +0000

    done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "shrestha@google.com"
+var accountPassword = "12345"
+accountCity = "Kolkata"
+let accountState;
+
+// accountId = 2
+accountEmail = "sm@sm.com"
+accountPassword = "121211"
+accountCity = "Mumbai"
+
+console.table([accountId , accountEmail , accountPassword , accountCity , accountState]);
