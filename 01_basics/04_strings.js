d935046cafa025f26a95270d760819a07092a6d7
Shrestha Mukherjee <134909641+Shrestha005@users.noreply.github.com>
2024-06-16 12:11:56 +0000

    string

---
+++ 01_basics/04_strings.js
@@ -0,0 +1,9 @@
+// console.log("hello" + " world")
+const name = "shrestha"
+const age = "20"
+
+console.log(`hello my name is ${name} and my age is ${age}`);  
+
+gamename = "shrestha"
+const newgamename = gamename.substring(0 , 4)
+console.log(newgamename);
