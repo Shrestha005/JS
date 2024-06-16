3469167751bcdb9a40ee02fc0a0f0ac4b0d8936e
Shrestha Mukherjee <134909641+Shrestha005@users.noreply.github.com>
2024-06-16 13:26:00 +0000

    str

--- 01_basics/04_strings.js
+++ 01_basics/04_strings.js
@@ -7,3 +7,12 @@ console.log(`hello my name is ${name} and my age is ${age}`);
 gamename = "shrestha"
 const newgamename = gamename.substring(0 , 4)
 console.log(newgamename);
+
+
+const url = "https://shres.github%20js"
+console.log(url.replace('%20' , '-'));
+console.log(url.includes('dude'));
+// node 01_basics/04_strings
+
+let newname = "shres-git-hub"
+console.log(newname.split('-'));
