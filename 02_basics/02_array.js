297cc16cf1dbb047c115fb86b4598d108543477d
Shrestha Mukherjee <134909641+Shrestha005@users.noreply.github.com>
2024-06-18 13:19:14 +0000

    commit

---
+++ 02_basics/02_array.js
@@ -0,0 +1,19 @@
+const marvel = ["thor" , "ironman" , "spiderman"]
+const dc = ["superman" , "flash" , "batman"]
+// marvel.push(dc)
+// console.log(marvel);
+
+// const all = marvel.concat(dc)
+// console.log(all);
+
+// const allheroes = [...marvel , ...dc]
+// console.log(allheroes);
+
+// console.log(Array.from("Piggy"));
+
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1 , score2 , score3));
