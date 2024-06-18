96191aa36b3bafb3e12dd5b2d3b0b55e267cad3f
Shrestha Mukherjee <134909641+Shrestha005@users.noreply.github.com>
2024-06-18 11:57:01 +0000

    commit

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -1,1 +1,11 @@
 //array
+
+const myArr = [9 , 1 , 2 , 3 , 4 , 5 , true , "shres"]
+const myArr2 = new Array(10 , 20 , 30 , 40 , 50 , 60 , 70)
+
+// console.log(myArr2[0]);
+
+//methods
+myArr2.push(5)
+myArr2.pop()
+console.log(myArr2);
