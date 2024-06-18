57c25655b727e6f2d9b7ad76b4fba010862b1a35
Shrestha Mukherjee <134909641+Shrestha005@users.noreply.github.com>
2024-06-18 12:30:25 +0000

    commit

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -8,4 +8,18 @@ const myArr2 = new Array(10 , 20 , 30 , 40 , 50 , 60 , 70)
 //methods
 myArr2.push(5)
 myArr2.pop()
+myArr2.unshift(11)
+myArr2.shift()
+// console.log(myArr2);
+// console.log(myArr2.includes(21));
+// console.log(myArr2.indexOf(40));
+
+const newArr = myArr2.join()
+
 console.log(myArr2);
+console.log(newArr);
+
+
+//slice and splice operation on array
+//slice - does not include range values and does not change original array
+//splice - includes range values and manipulates the original array
