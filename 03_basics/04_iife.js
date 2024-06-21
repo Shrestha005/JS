5ecebdb967a360736ab75d6e1715d0c804a5e048
Shrestha Mukherjee <134909641+Shrestha005@users.noreply.github.com>
2024-06-21 06:54:43 +0000

    commit

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,1 +1,10 @@
 //immediately invoked function expressions (iife)
+(function chai() {
+    console.log(`DB CONNECTED`);
+})();
+//this type of function call need ; to end
+
+//type2
+((name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+}) ('shres')
