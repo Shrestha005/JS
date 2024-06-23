7158bbb127fd4528765cabbfd5afa18fb54b3c15
Shrestha Mukherjee <134909641+Shrestha005@users.noreply.github.com>
2024-06-23 12:43:34 +0000

    commit

---
+++ 05_iteration/3.js
@@ -0,0 +1,20 @@
+//for of
+
+const arr = [1,2,3,4,5]
+
+for (const num of arr) {
+    console.log(num);
+}
+
+//iteration in maps
+//maps
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('FR', "France")
+
+// console.log(map);
+
+for (const [key, value] of map) {
+    console.log(key , ':-' , value);
+}
