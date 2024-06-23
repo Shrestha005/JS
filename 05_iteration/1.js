80d0d2ddea6c2ef3df1a2784e59f0545480792c0
Shrestha Mukherjee <134909641+Shrestha005@users.noreply.github.com>
2024-06-23 11:22:08 +0000

    commit

---
+++ 05_iteration/1.js
@@ -0,0 +1,34 @@
+// for
+
+for (let index = 0; index <= 10; index++) {
+    const element = index;
+    // console.log(element);
+}
+
+
+for (let i = 0; i <= 10; i++) {
+    // console.log(`Outer loop value : ${i}`);
+    for (let j = 0; j <= 10; j++){
+        // console.log(i + '*' + j + '=' + i*j);
+    }
+}
+
+let myarr = ["flash" , "batman" , "superman"]
+for (let i = 0; i < myarr.length; i++) {
+    const element = myarr[i];
+    // console.log(element);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
