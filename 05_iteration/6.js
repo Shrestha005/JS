0c892899ae9d73a3e770529606440a4e171f9812
Shrestha Mukherjee <134909641+Shrestha005@users.noreply.github.com>
2024-06-24 11:00:00 +0000

    commit

--- 05_iteration/6.js
+++ 05_iteration/6.js
@@ -1,9 +1,30 @@
 const coding = ["js","rb","py","java","cpp"]
 
 const values = coding.forEach((item) => {
-    console.log(item);
+    // console.log(item);
 });
 
-console.log(values);
+// console.log(values);
+
+const mynum = [1,2,3,4,5,6,7,8,9,10]
+//filter method
+const newNum = mynum.filter((num) => num>4)
+//or
+// const newnum = mynum.filter((num) => {return num>4})
+//because if scope is opened return must be used
+console.log(newNum);
+
+//foreach method
+const newnum= []
+
+mynum.forEach((num) => {
+    if (num>4) {
+        newnum.push(num)
+    }
+});
+
+
+
+
 
 //05_iteration/6.js
