e8edb3246adfdb50f82ab0533819d1118df93272
Shrestha Mukherjee <134909641+Shrestha005@users.noreply.github.com>
2024-06-26 11:22:24 +0000

    commit

---
+++ 08_events/1-eventbasics.js
@@ -0,0 +1,40 @@
+// // document.getElementById('owl').onclick = function(){
+// //     alert("owl click")
+// // }
+// document.getElementById('images'). addEventListener('click' , function(e){
+//     console.log("clicked inside the ul");
+// } , false)
+// document.getElementById('owl').addEventListener('click' , function(e){
+//     console.log("owl clicked");
+//     e.stopPropagation()
+// } , false)
+// document.getElementById('google').addEventListener('click' , function(e){
+//     e.preventDefault()
+//     e.stopPropagation()
+//     console.log("google clicked");
+// } , false)
+
+
+document.querySelector('#images').addEventListener('click' , function(e){
+    console.log(e.target.parentNode);
+    if (e.target.tagName === 'IMG') {
+        console.log(e.target.id);
+        let removeIt = e.target.parentNode
+        removeIt.remove()
+    }
+    // or
+    // removeIt.parentNode.removeChild(removeIt)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
